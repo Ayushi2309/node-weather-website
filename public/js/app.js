c901e8b5a8c6a3d193b90cc40033a7c572252805
Ayushi Vats <ayu23vats@gmail.com>
2020-01-11 12:03:18 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,7 @@
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if(data.error) {
-//             return console.log(data.error)
-//         }
-//         else {
-//             console.log(data.forecast)
-//             console.log(location)
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-//messageOne.textContent('From javascript')
 
 weatherForm.addEventListener(('submit'), (e) => {
     e.preventDefault()
@@ -38,7 +18,7 @@ weatherForm.addEventListener(('submit'), (e) => {
         }
         else {
             messageOne.textContent = data.location
-            messageOne.textContent = data.forecast
+            messageTwo.textContent = data.forecast
         }
     })
 })
